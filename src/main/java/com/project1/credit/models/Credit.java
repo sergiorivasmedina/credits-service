715ada794db60586eb34b0ff82f9394091775794
Sergio Andre Rivas Medina <srivasme@everis.com>
2020-07-12 11:33:57 -0500

    :sparkles: adding limit attribute

--- src/main/java/com/project1/credit/models/Credit.java
+++ src/main/java/com/project1/credit/models/Credit.java
@@ -16,18 +16,21 @@ public class Credit {
     private Double consumedAmount;
     private String creditType;
     @Nullable
+    private String limit;
+    @Nullable
     private List<String> creditTransactions;
 
     public Credit() {
     }
 
-    public Credit(String idCredit, String idCustomer, String idCurrency, Double availableAmount, Double consumedAmount, String creditType, List<String> creditTransactions) {
+    public Credit(String idCredit, String idCustomer, String idCurrency, Double availableAmount, Double consumedAmount, String creditType, String limit, List<String> creditTransactions) {
         this.idCredit = idCredit;
         this.idCustomer = idCustomer;
         this.idCurrency = idCurrency;
         this.availableAmount = availableAmount;
         this.consumedAmount = consumedAmount;
         this.creditType = creditType;
+        this.limit = limit;
         this.creditTransactions = creditTransactions;
     }
 
@@ -79,12 +82,76 @@ public class Credit {
         this.creditType = creditType;
     }
 
+    public String getLimit() {
+        return this.limit;
+    }
+
+    public void setLimit(String limit) {
+        this.limit = limit;
+    }
+
     public List<String> getCreditTransactions() {
         return this.creditTransactions;
     }
 
     public void setCreditTransactions(List<String> creditTransactions) {
         this.creditTransactions = creditTransactions;
+    }
+
+    public Credit idCredit(String idCredit) {
+        this.idCredit = idCredit;
+        return this;
+    }
+
+    public Credit idCustomer(String idCustomer) {
+        this.idCustomer = idCustomer;
+        return this;
+    }
+
+    public Credit idCurrency(String idCurrency) {
+        this.idCurrency = idCurrency;
+        return this;
+    }
+
+    public Credit availableAmount(Double availableAmount) {
+        this.availableAmount = availableAmount;
+        return this;
+    }
+
+    public Credit consumedAmount(Double consumedAmount) {
+        this.consumedAmount = consumedAmount;
+        return this;
+    }
+
+    public Credit creditType(String creditType) {
+        this.creditType = creditType;
+        return this;
+    }
+
+    public Credit limit(String limit) {
+        this.limit = limit;
+        return this;
+    }
+
+    public Credit creditTransactions(List<String> creditTransactions) {
+        this.creditTransactions = creditTransactions;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Credit)) {
+            return false;
+        }
+        Credit credit = (Credit) o;
+        return Objects.equals(idCredit, credit.idCredit) && Objects.equals(idCustomer, credit.idCustomer) && Objects.equals(idCurrency, credit.idCurrency) && Objects.equals(availableAmount, credit.availableAmount) && Objects.equals(consumedAmount, credit.consumedAmount) && Objects.equals(creditType, credit.creditType) && Objects.equals(limit, credit.limit) && Objects.equals(creditTransactions, credit.creditTransactions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idCredit, idCustomer, idCurrency, availableAmount, consumedAmount, creditType, limit, creditTransactions);
     }
 
     @Override
@@ -96,6 +163,7 @@ public class Credit {
             ", availableAmount='" + getAvailableAmount() + "'" +
             ", consumedAmount='" + getConsumedAmount() + "'" +
             ", creditType='" + getCreditType() + "'" +
+            ", limit='" + getLimit() + "'" +
             ", creditTransactions='" + getCreditTransactions() + "'" +
             "}";
     }
